c6186145c554c57b6f2e3d99bfb7c7c8dcf0bedc
Pedro Miguel <93842972+PedroMiguel7@users.noreply.github.com>
2022-11-15 19:24:55 -0300

    refactor: changing route names for better display

--- service/src/routes/accounts.ts
+++ service/src/routes/accounts.ts
@@ -2,9 +2,9 @@ import Router from "express";
 import { authMiddleware } from "../middlewares/authMiddleware";
 import { AccountController } from "../controllers/AccountController";
 
-const routes = Router();
+const routesaccount = Router();
 
-routes.get("/account/:id", authMiddleware, new AccountController().ListAccount);
-routes.delete("/account/:id", authMiddleware, new AccountController().ListAccount);
+routesaccount.get("/account/:id", authMiddleware, new AccountController().ListAccount);
+routesaccount.delete("/account/:id", authMiddleware, new AccountController().ListAccount);
 
-export default routes;
+export default routesaccount;
